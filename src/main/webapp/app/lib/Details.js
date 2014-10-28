d94c4d8e583266bdd136f962935a284d6af92757
trieck <trieck@gmail.com>
2014-10-28 17:40:04 -0400

    Details tab panel

--- src/main/webapp/app/lib/Details.js
+++ src/main/webapp/app/lib/Details.js
@@ -1,21 +1,27 @@
 Ext.define('bible.lib.Details', {
-    extend: 'Ext.view.View',
+    extend: 'Ext.tab.Panel',
     alias: 'widget.details',
-    title: 'Details',
-    id: "detailsView",
-    collapsible: true,
     split: true,
     width: 300,
-    padding: '5',
-    tpl: [
-        '<tpl for=".">',
-        '<h2>{book} {chapter}:{verse}</h2>',
-        '<div>{text}</div><br/>',
-        '</tpl>'
-    ],
+    layout: 'fit',
+
+    items: [ {
+        xtype: 'dataview',
+        padding: '5',
+        title: 'Details',
+        tpl: [
+            '<tpl for=".">',
+            '<h2>{book} {chapter}:{verse}</h2>',
+            '<div>{text}</div><br/>',
+            '</tpl>' ],
+        store: 'Detail'
+    } ],
 
     initComponent: function () {
-        this.store = Ext.getStore("Detail");
+        this.tabBar = {
+            border: true
+        };
+
         this.callParent(arguments);
     }
 });
